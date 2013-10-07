a8aa8183956fd8fae55ff61a8873ad301dc43549
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-10-07 15:14:17 -0400

    Fixes issues with Session vs Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
-class SessionController < ApplicationController
-  before_filter :require_no_current_user!, only: [:create, :new]
-  before_filter :require_current_user1, only: :destroy
+class SessionsController < ApplicationController
+  before_filter :require_no_current_user!, :only => [:create, :new]
+  before_filter :require_current_user!, :only => [:destroy]
 
   def create
     user = User.find_by_credentials(
       params[:user][:username],
-      params[:user][:password])
+      params[:user][:password]
+    )
 
     if user.nil?
-      render :json => "Incorrect username or password"
+      render :json => "Credentials were wrong"
     else
       self.current_user = user
       redirect_to user_url(user)
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,4 @@
-module SessionHelper
+module SessionsHelper
   def current_user
     User.find_by_session_token(session[:session_token])
   end
@@ -8,7 +8,7 @@ module SessionHelper
     session[:session_token] = user.session_token
   end
 
-  def logout_current_user
+  def logout_current_user!
     current_user.reset_session_token!
     session[:session_token] = nil
   end
