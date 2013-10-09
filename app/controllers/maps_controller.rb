ed88f2b513fc80946f089c3f4000d48d5a9a6b90
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-10-09 14:23:37 -0400

    Maps and restaurants can be edited

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,22 @@
 class MapsController < ApplicationController
+  def show
+    @map = Map.includes(:restaurants).find(params[:id])
+    @restaurants = @map.restaurants
+    @restaurant = @restaurants.first
+    @finished, @unfinished = [], []
+    @map.restaurants.each do |rest|
+      rest.completed ? @finished << rest : @unfinished << rest
+    end
+
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @restaurants }
+    end
+  end
+
+  def index
+  end
+
   def new
     @map = Map.new
     3.times { @map.restaurants.build }
@@ -48,31 +66,41 @@ class MapsController < ApplicationController
   end
 
   def update
-  end
+      begin
+        ActiveRecord::Base.transaction do
+          @map = Map.find(params[:id])
+          @restaurants = []
+
+          params[:map][:restaurants_attributes].each do |r, data|
+
+            address = data[:address]
+            coords = convert_address(address)
+
+            data.delete_if{ |k| k == "_destroy" }
+            data["latitude"] = coords[0]
+            data["longitude"] = coords[1]
+
+            restaurant = Restaurant.find(data[:id])
+            restaurant.update_attributes(data)
+          end
+
+          @map.update_attributes(params[:map])
+
+          raise "invalid" unless @map.valid? && @restaurants.all? { |obj| obj.valid? }
+        end
+      rescue
+        render :edit
+      else
+        flash[:notice] = "Successfully updated map."
+        redirect_to @map
+      end
+    end
 
   def destroy
     @map = Map.find(params[:id])
     @map.destroy
     flash[:notice] = "Successfully deleted map"
     redirect_to user_url(current_user)
-  end
-
-  def show
-    @map = Map.includes(:restaurants).find(params[:id])
-    @restaurants = @map.restaurants
-    @restaurant = @restaurants.first
-    @finished, @unfinished = [], []
-    @map.restaurants.each do |rest|
-      rest.completed ? @finished << rest : @unfinished << rest
-    end
-
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @restaurants }
-    end
-  end
-
-  def index
   end
 
 end
