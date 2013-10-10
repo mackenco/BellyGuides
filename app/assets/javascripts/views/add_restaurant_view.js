b40576d494205b5caa60dd4df7f0ad57dd0d99c2
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-10-10 16:29:30 -0400

    Adding a restaurant create a new record

--- app/assets/javascripts/views/add_restaurant_view.js
+++ app/assets/javascripts/views/add_restaurant_view.js
@@ -20,9 +20,9 @@ BellyGuide.Views.AddRestaurantView = Backbone.View.extend({
 
   add: function (event) {
     event.preventDefault();
-    var new_rest = this.model
+    var new_rest = this.model.clone()
     var formData = $(event.currentTarget.form).serializeJSON()
-    new_rest.set({map_id: parseInt(formData.map_id)})
+    new_rest.set({map_id: parseInt(formData.map_id), completed: false, note: ""})
     new_rest.save();
     alert("Added!");
     Backbone.history.navigate("#/");
