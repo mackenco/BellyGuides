ad4e7efaee431a72068c8b32ab0c80a3c64b4411
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-10-10 11:52:16 -0400

    Users cannot edit checkboxes of others' maps

--- app/assets/javascripts/belly_guide.js
+++ app/assets/javascripts/belly_guide.js
@@ -3,9 +3,10 @@ window.BellyGuide = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function($rootEl, restaurantData, mapID) {
+  initialize: function($rootEl, restaurantData, mapID, owner) {
     BellyGuide.restaurants = new BellyGuide.Collections.Restaurants(restaurantData)
     BellyGuide.mapID = mapID;
+    BellyGuide.owner = owner;
     new BellyGuide.Routers.RestaurantsRouter($rootEl);
     Backbone.history.start();
   }
