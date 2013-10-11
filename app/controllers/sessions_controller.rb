2172e997bb06c01281284eb24370768a359de4c2
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-10-11 15:58:05 -0400

    Styling tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,11 +12,13 @@ class SessionsController < ApplicationController
       render :json => "Credentials were wrong"
     else
       self.current_user = user
+      flash.notice = "Welcome back #{current_user}"
       redirect_to user_url(user)
     end
   end
 
   def destroy
+    flash.notice = "Goodbye #{current_user}"
     logout_current_user!
     redirect_to new_session_url
   end
