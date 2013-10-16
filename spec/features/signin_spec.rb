6f79cb54cb70a2fad58a134e04fe47c98542e5f6
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-10-16 17:22:09 -0400

    Starting work with factory girl

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -5,9 +5,14 @@ describe "the signin process", type: :feature do
     User.create(username: "test", email: "test@example.com", password: "test")
   end
 
-  it "signs me in" do
+  it 'has a sign in page' do
+    visit new_session_url
+    expect(page).to have_content 'Sign In'
+  end
+
+  it 'signs me in' do
     visit '/session/new'
-    within("form.sign-in") do
+    within('form.sign-in') do
       fill_in 'user_username', with: 'test'
       fill_in 'user_password', with: 'test'
     end
